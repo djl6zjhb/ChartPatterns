07546f2429180e5ec96580650a3582e318219636
Daniel Lagalante <daniel.lagalante@volta-medical.com>
2025-12-02 23:57:36 -0500

    docstring

--- label_events.py
+++ label_events.py
@@ -1,9 +1,15 @@
 import pandas as pd
-from pos_to_date import pos_to_date
 
 def label_events(dt_candidates: pd.DataFrame, dt_events: pd.DataFrame) -> pd.DataFrame:
     """
     Label each candidate in dt_candidates as True if it is in dt_events, else False.
+
+    Parameters
+    ----------
+    dt_candidates : pd.DataFrame
+        DataFrame of candidate double top events with 'peak1_date', 'trough_date', 'peak2_date' columns.
+    dt_events : pd.DataFrame
+        DataFrame of confirmed double top events with 'peak1_date', 'trough_date', 'peak2_date' columns.
     """
     dt_events = dt_events.set_index(['peak1_date', 'trough_date', 'peak2_date'])
     dt_candidates = dt_candidates.set_index(['peak1_date', 'trough_date', 'peak2_date'])
