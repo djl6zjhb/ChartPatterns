701fb5172e0de1dcb81ae65157113f427ae9e466
Daniel Lagalante <daniel.lagalante@volta-medical.com>
2025-12-03 21:00:31 -0500

    adjusted to handle predicted double top events

--- compute_forward_returns.py
+++ compute_forward_returns.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def compute_forward_returns(df, events_df, horizons=(5, 20, 60)):
+def compute_forward_returns(df, events_df, predicted = False, horizons=(5, 20, 60)):
     """
     For each event (by confirm_date), compute forward returns at the given horizons.
     Returns are simple percentage returns: (Close[t+H] / Close[confirm]) - 1.
@@ -11,6 +11,8 @@ def compute_forward_returns(df, events_df, horizons=(5, 20, 60)):
         DataFrame containing stock price data for a single ticker.
     events_df : pd.DataFrame
         DataFrame of confirmed double top events with 'confirm_date' column.
+    predicted : bool
+        If True, indicates that events_df contains predicted events.
     horizons : tuple of int
         Forward-return horizons in trading days.
     """
@@ -23,7 +25,10 @@ def compute_forward_returns(df, events_df, horizons=(5, 20, 60)):
         for _, row in events_df.iterrows():
             
             # index at double top confirmation date
-            t0 = row['confirm_date']
+            if predicted:
+                t0 = row['peak2_pos'] + 1 # returns must be calculated from the first day after closing at a second peak
+            else:
+                t0 = row['confirm_date']
 
             # index at the given horizonif t0 + h is within df index range; edge case to avoid erroring out
             t_fwd = df.index[df.index.get_loc(t0) + h] if (df.index.get_loc(t0) + h) < len(df.index) else None
